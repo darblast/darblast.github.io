f8a0fa15b505f1d3e2aade6358b25cb1330ef798
Alberto La Rocca <a71104@gmail.com>
2020-04-11 14:36:26 +0100

    Customization

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {
+  Container,
+  Jumbotron,
+} from 'react-bootstrap';
+
+
+export default function () {
+  return (
+    <Jumbotron fluid>
+      <Container>
+        <h1>Darblast</h1>
+      </Container>
+    </Jumbotron>
+  );
+};
